a461435052967aab4e4821761635ea48d30b47d7
Duvan Carvajal <h_carvajal@outlook.es>
2020-10-12 14:26:26 -0500

    credenciales para conectar db

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
   database: {
-    host: process.env.BaseDatosConexion_URL,
-    user: process.env.BaseDatosConexion_USER,
-    password: process.env.BaseDatosConexion_PASS,
-    database: process.env.BaseDatosConexion_DBNOMBRE,
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'b24d9d3069a88d',
+    password: 'c15904b3',
+    database: 'heroku_dcc235497bdd5d0',
   },
 };
